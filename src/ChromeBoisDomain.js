53a34a51b2ba558e6eacfdd422946dfa9c18776a
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 08:58:41 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,7 +10,9 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-     handleMouseMove = event
+     handleMouseMove = (event) => {
+       
+     }
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
