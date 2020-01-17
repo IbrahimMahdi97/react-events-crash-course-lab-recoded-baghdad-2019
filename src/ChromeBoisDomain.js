bc6fe16c97712cba7aa7c748e225fcfd08391655
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 09:02:57 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -20,7 +20,7 @@ export default class ChromeBoisDomain extends Component {
    */
    handleKeyDown = (event) => {
      if (event.key === 'a') {
-       resize ('+') else if ()
+       resize ('+') else if (event.key === '')
    }
    
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
