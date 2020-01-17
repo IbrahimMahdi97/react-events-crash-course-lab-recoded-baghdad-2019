775d9ec0dda1cc4cb814d25f678993507494d5a5
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 09:03:19 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -21,7 +21,7 @@ export default class ChromeBoisDomain extends Component {
    handleKeyDown = (event) => {
      if (event.key === 'a') {
        resize ('+') else if (event.key === 's') {
-         
+         resize('-')
        }
    }
    
